f644bb338f820b461bf224066884c42cb69acbcb
Matthew Elliott <mse951993@gmail.com>
2019-03-01 17:22:22 -0800

    Got answer checking working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,38 +2,61 @@
 
 var timeRemainingText = $("#timeRemainingText");
 var mainContainer = $("#mainContainer");
+var startButton = $("#startButton");
+var correctAnswers = 0;
+var wrongAnswers = 0;
 
 //I realize using an object for the counter is a little over-complicated, but I felt
 //like doing it... So here we are.
+//removed all uses of "this" because some of the functions are called by an interval, which doesn't work with "this"
 var counter = {
     countDown: 60,
     counterID: 0,
     startCountdown: function () {
-        this.counterID = setInterval(this.countDownIncrement, 1000);},
+        clearInterval(counter.counterID);
+        counter.counterID = setInterval(counter.countDownIncrement, 1000);},
     countDownIncrement: function () {
-        this.countDown--;},
+        //countdown and update text
+        counter.countDown--;
+        timeRemainingText.text(counter.countDown);
+        //check if game is over because of time
+        if (counter.countDownFinished()) {
+            endGame();
+        }},
+    //Yea, I could just have this function endGame() instead of returning true 
+    //or false, but I wanted to return something
     countDownFinished: function () {
-        if (this.countDown <= 0) {
+        if (counter.countDown <= 0) {
             return true;
         }
         return false;}
 }
+console.log(counter.countDown);
 
-//Kinda done: make an array of questions with correct and incorrect answers
+//make an array of questions with correct and incorrect answers
+//TODO make questions and answers.
+//format is as follows:
+//[QUESTION,
+//ANSWER CHOICE 1,
+//ANSWER CHOICE 2,
+//ANSWER CHOICE 3,
+//ANSWER CHOICE 4,
+//CORRECT ANSWER]
+//correct answer should be the same exact string as the right answer choice
 var questions = [
     ["1 INSERT QUESTION HERE",
     "11 INSERT ANSWER HERE",
     "12 INSERT ANSWER HERE",
     "13 INSERT ANSWER HERE",
     "14 INSERT ANSWER HERE",
-    "1 INSERT CORRECT ANSWER HERE"],
+    "11 INSERT ANSWER HERE"],
 
     ["2 INSERT QUESTION HERE",
     "21 INSERT ANSWER HERE",
     "22 INSERT ANSWER HERE",
     "23 INSERT ANSWER HERE",
     "24 INSERT ANSWER HERE",
-    "2 INSERT CORRECT ANSWER HERE"],
+    "21 INSERT ANSWER HERE"],
 
     ["3 INSERT QUESTION HERE",
     "31 INSERT ANSWER HERE",
@@ -67,32 +90,62 @@ function displayQuestions () {
         var aCol = $("<div class='col-md-12'>")
         aRow.append(aCol);
         //make a form and append it to the coloumn
-        var form = $("<form name='question-" + i + "'>");
+        var form = $("<form id='question-" + i + "' name='question-" + i + "'>");
         aCol.append(form);
         //loop through all answers for current question
         for (var ii = 1; ii < 5; ii++) {
             //make a button and a label for each answer and append each to the form.
             //button IDs are answer-1, answer-2 and so on
             //button names are answers
-            var button = $("<input type='radio' name='answers' id='answer-" + ii + "' value='" + questions[i][ii] + "'>");
+            var button = $("<input class='answer' type='radio' name='answers' id='answer-" + ii + "' value='" + questions[i][ii] + "'>");
             form.append(button);
-            var label = $("<label for='answer-" + ii + "'>");
+            var label = $("<label class='' for='answer-" + ii + "'>");
             label.text(questions[i][ii]);
             form.append(label);
         }
     }
 
 }
-displayQuestions();
 
+//Start the game when start button is pressed, then delete the start button
+function startGame() {
+    displayQuestions();
+    counter.startCountdown();
+    $("#startButtonRow").remove();
+}
 
-//greet user with a start screen and have a button to start the game
+//Ends the game and tallies score
+//TODO endgame function
+function endGame () {
+    console.log("Game over!");
+}
 
-//when counter hits 0, end game
+function checkAnswers() {
+    for (var i = 0; i < questions.length; i++) {
+        //Gets back an array containing the button elements for the question
+        var buttonsArray = $("#question-" + i).children(".answer");
+        //Checks each button to see if it's been checked AND if the checked buttons 
+        //value matches the answer of that question, then increments the correctAnswers
+        //or the wrong answers. I condensed it to take up fewer lines, I know it's a
+        //little hard to read because of that. If you format the doc it should be better
+        for (var ii = 0; ii < 4; ii++) {
+            if (buttonsArray[ii].checked && buttonsArray[ii].value === questions[i][5]) {
+                correctAnswers++;
+            } else { wrongAnswers++; }
+        }
+    }
+}
 
-//make button to end game and submit answers
+//set the time remaining text to show before you actually start the timer
+timeRemainingText.text(counter.countDown);
 
+//Hopefully you can figure this one out
+startButton.click(startGame);
 
+$(document).keyup(function () {
+    checkAnswers();
+    console.log(correctAnswers + "   " + wrongAnswers);
+})
 
 
 //when games ends, tally correct and incorrect answers and unanswered questions
